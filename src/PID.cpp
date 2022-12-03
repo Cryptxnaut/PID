fa222f6f17100c639f052de9c53d04724d708eb8
Jai Akilesh <jaiakilesh06@gmail.com>
2022-12-03 18:48:30 +0000

    Declare variables

--- src/PID.cpp
+++ src/PID.cpp
@@ -6,6 +6,18 @@
 
 double wheelDiameter = 3.25;
 double distance;
+////////////////////////////
+double kP = 0.0;
+double kI = 0.0;
+double kD = 0.0;
+
+double error;
+double prevError = 0;
+double proportional;
+double intergral;
+double derivative;
+
+
 
 double getDesiredRotations(wheelDiameter, distance){
   double circumference = (M_PI * wheelDiameter);
